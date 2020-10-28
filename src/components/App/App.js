de1165957e173aa4b182c417649e0f207cef31a9
Myron <dubtwee@gmail.com>
2020-10-28 16:11:43 -0500

    live solving for Track Runner base mode

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,12 +2,54 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  state = {
+    milesRan: 0,
+    recentRun: 0,
+    longestRun: 0,
+  };
+
+  handleSubmitRun = (event) => {
+    event.preventDefault();
+
+    // previous longest run this.state.longestRun
+    // new run this.state.milesRan
+    let newLongestRun = this.state.longestRun;
+
+    if (this.state.milesRan > this.state.longestRun) {
+      newLongestRun = this.state.milesRan;
+    }
+
+    this.setState({
+      recentRun: this.state.milesRan,
+      longestRun: newLongestRun,
+    });
+  };
+
+  handleMilesChange = (event) => {
+    console.log(event.target.value);
+    this.setState({
+      milesRan: parseInt(event.target.value),
+    });
+  };
+
   render() {
     return (
       <div>
         <header>
           <h1>React Track Runner</h1>
         </header>
+
+        <form onSubmit={this.handleSubmitRun}>
+          <input
+            type="number"
+            placeholder="Miles Ran"
+            onChange={this.handleMilesChange}
+          />
+          <button>AddNewRun</button>
+        </form>
+
+        <p>Recent Run: {this.state.recentRun}</p>
+        <p>Longest Run: {this.state.longestRun}</p>
       </div>
     );
   }
